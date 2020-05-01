31e1ef4c6c2a0ee857ab6588bf548f709ab2d85a
alessio <alessiochen98@gmail.com>
2020-05-01 11:28:18 +0200

    Dataset

--- Dataset.py
+++ Dataset.py
@@ -20,35 +20,34 @@ class Dataset:
                     self.dataset[i][self.ordedered_array[j].value] = 1
 
         mat = np.asmatrix(self.dataset)
-        np.savetxt('out.txt', mat, fmt='%s')
+        names = ''
+        for i in range(len(self.ordedered_array)):
+            names = names + ' ' + self.ordedered_array[i].name
 
-
-
-
+        np.savetxt('out.txt', mat, header=names, fmt='%s')
 
     def get_prob(self, i, k):
         # k è i'indice del nodo che sto esaminando ordinato in base al DFS
         # i è l'indice del nodo padre
         prob = 0
-
         if len(self.net.nodes[k].parents) == 0:  # prior p(a)
             prob = self.net.nodes[k].cpt[1]
 
         if len(self.net.nodes[k].parents) == 1:  # nodo con un padre p(T|A)
             if self.dataset[i][self.net.nodes[k].parents[0]] == 0:
-                prob = self.net.nodes[k].cpt[1][0]  # A =  f
+                prob = self.net.nodes[k].cpt[1][0]
             else:
-                prob = self.net.nodes[k].cpt[0][1]  # A =  t
+                prob = self.net.nodes[k].cpt[0][1]
 
         if len(self.net.nodes[k].parents) == 2:  # nodo con 2 padri p(E|T,L)
-            if self.dataset[i][self.net.nodes[k].parents[0]] == 0:  # T = f
-                if self.dataset[i][self.net.nodes[k].parents[1]] == 0:  # L = f
+            if self.dataset[i][self.net.nodes[k].parents[0]] == 0:
+                if self.dataset[i][self.net.nodes[k].parents[1]] == 0:
                     prob = self.net.nodes[k].cpt[3][0]
                 else:
-                    prob = self.net.nodes[k].cpt[1][0]  # T = f & L = t
+                    prob = self.net.nodes[k].cpt[1][0]
 
-            if self.dataset[i][self.net.nodes[k].parents[1]] == 1:  # L = T
-                if self.dataset[i][self.net.nodes[k].parents[0]] == 1:  # L = T & T = t
+            if self.dataset[i][self.net.nodes[k].parents[1]] == 1:
+                if self.dataset[i][self.net.nodes[k].parents[0]] == 1:
                     prob = self.net.nodes[k].cpt[0][0]
                 else:
                     prob = self.net.nodes[k].cpt[2][0]
